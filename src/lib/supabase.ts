00a35e2d845475a788b7fbc23bd6d18232817dd3
Oyunbat <oyunbat9958@gmail.com>
2025-06-04 23:28:20 +0800

    broadcast der anon key orj ireguiges blod deploy unasan tged anon keyg verceling env variable der hucher zaj ugud bolgoson

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,10 @@
 // src/lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Anon Key:", supabaseAnonKey ? "Set" : "Not Set");
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   realtime: {
     params: {
